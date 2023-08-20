fce30d6464dfeeb2cf223c50fb97ee00d444fbeb
Vagner Elena <dipnoe@gmail.com>
2023-08-21 02:59:57 +0300

    add cache

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
+from django.core.cache import cache
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
 from blog.models import Blog
+from config.settings import CACHE_ENABLED
 
 
 # Create your views here.
@@ -11,6 +13,17 @@ class BlogView(ListView):
 
 class BlogDetailView(DetailView):
     model = Blog
+
+    def cache(self):
+        if CACHE_ENABLED:
+            key = f'blog_list_{self.object.pk}'
+            blog_list = cache.get(key)
+            if blog_list is None:
+                blog_list = Blog.objects.all()
+                cache.set(key, blog_list)
+                return blog_list
+        return Blog.objects.all()
+
 
     def get_object(self, queryset=None):
         self.object = super().get_object(queryset)
