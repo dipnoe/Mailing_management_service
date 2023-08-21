90b14a378cc87fada59ceacd897b9a97033ebd52
Vagner Elena <dipnoe@gmail.com>
2023-08-21 03:00:09 +0300

    fix

--- mailing/services.py
+++ mailing/services.py
@@ -1,37 +1,3 @@
-# import datetime
-#
-# from django.core.mail import send_mail
-#
-# from mailing.models import Message, Customer, Mailing, Log
-# from config import settings
-#
-#
-# def send_mailing(message: Message, mailing: Mailing):
-#     emails = []
-#     for customer in Customer.objects.all():
-#         emails.append(str(customer.email))
-#
-#     if (mailing.frequency == 'O' and
-#             datetime.datetime.fromisoformat(mailing.distribution_settings) <= datetime.datetime.now()):
-#         try:
-#             send_mail(
-#                 f'{message.title}',
-#                 f'{message.body}',
-#                 settings.EMAIL_HOST_USER,
-#                 [*emails]
-#             )
-#             log = Log.objects.create(last_try=datetime.datetime.now(), status='S', response='успешно')
-#             log.save()
-#             mailing.status = 'CO'
-#             mailing.save()
-#
-#         except Exception as ex:
-#             log = Log.objects.create(last_try=datetime.datetime.now(), status='UNS', response=ex)
-#             log.save()
-#     else:
-#         mailing.status = 'LA'
-#         mailing.save()
-
 from django.core.mail import send_mail
 from mailing.models import Message, Customer, Mailing, Log
 from config import settings
@@ -43,8 +9,7 @@ def send_mailing(message: Message):
 
     current_time = timezone.now().time()
 
-    if (message.mailing.frequency == 'O' and
-            message.mailing.distribution_settings <= current_time):
+    if message.mailing.status == 'CR' and message.mailing.distribution_settings > current_time:
 
         try:
             send_mail(
@@ -54,16 +19,15 @@ def send_mailing(message: Message):
                 emails
             )
             log = Log.objects.create(last_try=current_time, status='S', response='успешно')
-            print(message.mailing.status)
-            message.mailing.status = 'CO'
-            message.mailing.save()
+
 
         except Exception as ex:
             log = Log.objects.create(last_try=current_time, status='UNS', response=str(ex))
 
         finally:
+            if message.mailing.frequency == 'O':
+                message.mailing.status = 'CO'
+            else:
+                message.mailing.status = 'LA'
+            message.mailing.save()
             log.save()
-
-    else:
-        message.mailing.status = 'LA'
-        message.mailing.save()
